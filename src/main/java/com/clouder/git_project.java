e9c737b00f16ab2d5a30c05e8e6e3091c500315b
xiaofeng <xiaofengmic@hotmail.com>
2019-04-27 18:57:34 +0800

    3 new branch jessica

--- src/main/java/com/clouder/git_project.java
+++ src/main/java/com/clouder/git_project.java
@@ -8,5 +8,6 @@ public class git_project {
     public static void main(String[] args) {
         System.out.println("cerated by xiaofeng ...");
         System.out.println("fixed by xiaofeng ...");
+        System.out.println("new branch by Jessica ...");
     }
 }
